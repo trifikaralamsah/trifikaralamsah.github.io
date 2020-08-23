fcdf638b34ec928e5eb2fd15b4a06e947b2089e0
trifikaralamsah <trifikar24@gmail.com>
2020-08-23 23:43:15 +0700

    initial commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,15 @@
+@extends('layout/main')
+
+@section('title', 'Home Fikar Laravel') 
+
+@section('container')
+
+    <div class="container">
+     <div class="row">
+      <div class="col-10">
+      <h1 class="mt-3">Hello, world!</h1>
+      </div>
+     </div>
+    </div>
+
+@endsection
